22c77ffb16d34f935444d796e6af90239f032272
Kyah Rindlisbacher <kyah101@gmail.com>
2013-06-26 16:40:49 +1000

    Updates to the caching script

--- CrossDomain.php
+++ CrossDomain.php
@@ -1,22 +1,58 @@
 <?php
 
+$info = '';
+$json = array();
+$output = '';
+
+
 if (empty($_GET['url']) && empty($_GET['img'])) {
-	print '{error:"no args"}';
-	exit();
+	$json[] = '"error":"no args"';
+	$output = render_json($json);
 }
 elseif (!empty($_GET['url'])) {
 	// use $ because i can
-	print file_get_contents(urldecode($_GET['url']));	
+	$output = file_get_contents(urldecode($_GET['url']));	
 }
 elseif (!empty($_GET['img'])) {
 	$folder = 'img';
 	$file_name = basename($_GET['img']);
-	$file = __DIR__ . '/' . $folder . '/' . $file_name;
+	$file_name = urldecode(urldecode($file_name));
 
+	$info .= "__" . $url . '__';
+	
+	$file = __DIR__ . '/' . $folder . '/' . $file_name;
 	if (!file_exists($file)) {
-		$imgData = file_get_contents(urldecode($_GET['img']));	
-		file_put_contents($file, $imgData);
+		$url = $_GET['img'];
+		if ($_GET['urlencodings']) {
+			$encodes = (int) $_GET['urlencodings'];
+			if ($encodes < 10) {
+				while ($encodes > 0) {
+					$url = urldecode($url);
+					$encodes--;
+				}
+			}
+		}
+		$imgData = file_get_contents($url);	
+		$success = file_put_contents($file, $imgData);
+
+		$json[] = '"return":"' . $success . '"';
+	}
+	else {
+		$info .= 'Image exists.';
+	}
+	
+	$json[] = '"img":"http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $folder . '/' . $file_name .'"';
+
+	if ($info) {
+		$json[] = '"info":"' . $info . '"';
 	}
 
-	print '{"img":"http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $folder . '/' . $file_name .'"}';
+	$output = render_json($json);
 }
+
+function render_json ($json){
+	return '{' . implode(',', $json) . '}';
+}
+
+
+print $output; 
